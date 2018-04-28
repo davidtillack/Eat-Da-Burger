5c0a35493601bc48df012e7c6ad0b314b3930186
David <dstillac@ncsu.edu>
2018-04-28 15:14:13 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
-connection.connect(function(err) {
+connection.connect(err => {
   if (err) {
-    console.error("error in connecting: " + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
 
 module.exports = connection;
