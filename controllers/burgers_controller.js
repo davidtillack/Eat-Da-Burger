8abb7ef30bbe466c02fb6308ba02e4e59cfd8cea
David <dstillac@ncsu.edu>
2018-04-27 20:20:41 -0400

    variable change maybe messing with connections?

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,10 +7,10 @@ var burger = require("../models/burger.js");
 
 //Creating all the routes and seting up the logic within those routes
 router.get("/", function(req, res) {
-  res.redirect("/burger");
+  res.redirect("/burgers");
 });
 
-router.get("/burger", function(req, res) {
+router.get("/burgers", function(req, res) {
   burger.selectAll(function(burgerData) {
     res.render("index", { burger_data: burgerData });
   });
--- models/burger.js
+++ models/burger.js
@@ -3,25 +3,25 @@ var orm = require("../config/orm.js");
 
 var burger = {
   selectAll: function(response) {
-    orm.selectAll("burger", function(res) {
+    orm.selectAll("burgers", function(res) {
       response(res);
     });
   },
   // The variables cols and vals are arrays.
   insertOne: function(value, response) {
-    orm.insertOne("burger", "burger_name", value, function(res) {
+    orm.insertOne("burgers", "burger_name", value, function(res) {
       response(res);
     });
   },
   updateOne: function(objColVals, condition, response) {
-    orm.updateOne("burger", "devoured", objColVals, "id", condition, function(
+    orm.updateOne("burgers", "devoured", objColVals, "id", condition, function(
       res
     ) {
       response(res);
     });
   },
   deleteOne: function(condition, response) {
-    orm.deleteOne("burger", "id", condition, function(res) {
+    orm.deleteOne("burgers", "id", condition, function(res) {
       response(res);
     });
   }
