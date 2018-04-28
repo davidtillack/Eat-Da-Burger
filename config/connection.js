35726c4e8a90f4c15ac9655a22430be5732e0bff
David <dstillac@ncsu.edu>
2018-04-28 15:50:29 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,18 @@
-const mysql = require("mysql");
-var connection;
+var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-  });
-}
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
 
-connection.connect(err => {
+connection.connect(function(err) {
   if (err) {
-    console.error(`error connecting: ${err.stack}`);
+    console.error("error in connecting: " + err.stack);
     return;
   }
-  console.log(`connected as id ${connection.threadId}`);
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers
 (
-  ID int NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR
-  (255) NOT NULL,
+  ID INT NOT NULL,
+  burger_name VARCHAR (100) NULL,
   devoured BOOLEAN NOT NULL,
-  PRIMARY KEY
-  (ID)
+  PRIMARY KEY (ID)
 );
