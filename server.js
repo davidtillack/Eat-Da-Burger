7ae65924fe86a9a4e2092186db4681204e1bbbe9
David <dstillac@ncsu.edu>
2018-04-28 16:47:09 -0400

    test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080;
 
 // BodyParser makes it easy for our server to interpret data sent to it.
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
@@ -24,7 +24,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Static directory
-app.use(express.static("app/public"));
+app.use(express.static(__dirname + "/public"));
 
 // Listener
 app.listen(PORT, function() {
