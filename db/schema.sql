14d577ebe22e164f53a5795cf66010fc8b1803c1
David <dstillac@ncsu.edu>
2018-04-28 11:55:33 -0400

    test

--- db/schema.sql
+++ db/schema.sql
@@ -2,11 +2,10 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers
 (
-  ID int NOT NULL
-  AUTO_INCREMENT,
-	burger_name VARCHAR
+  ID int NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR
   (255) NOT NULL,
-  devoured BOOLEAN NOT NULL,	
-	PRIMARY KEY
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY
   (ID)
 );
