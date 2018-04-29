f65fdc5e3fffadff065aa2abf8b992a512369250
David <dstillac@ncsu.edu>
2018-04-29 16:45:41 -0400

    retried databases

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,11 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers
 (
-  ID INT NOT NULL,
-  burger_name VARCHAR (100) NULL,
+  ID INT NOT NULL
+  AUTO_INCREMENT,
+  burger_name VARCHAR
+  (100) NULL,
   devoured BOOLEAN NOT NULL,
-  PRIMARY KEY (ID)
+  PRIMARY KEY
+  (ID)
 );
