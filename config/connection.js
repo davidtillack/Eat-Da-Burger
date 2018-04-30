49e7079c6558b711a0851bf18f9fd0dde26163fa
David <dstillac@ncsu.edu>
2018-04-29 20:24:14 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,14 @@ var connection = mysql.createConnection({
   user: "root",
   password: "",
   database: "burgers_db",
-  socketPath: "/var/run/mysqld/mysqld.sock"
+  socketPath: "/var/run/mysql/mysql.sock"
+});
+
+console.log({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_SECRET,
+  database: process.env.MYSQL_DB
 });
 
 connection.connect(function(err) {
