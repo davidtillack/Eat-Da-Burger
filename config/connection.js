8388411507c5fb0ab03979e34eddf9966169b76e
David <dstillac@ncsu.edu>
2018-04-29 20:34:58 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
-  database: "burgers_db",
-  socketPath: "/var/run/mysql/mysql.sock"
+  database: "burgers_db"
 });
 
 console.log({
